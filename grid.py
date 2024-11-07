49a7cd6040b5674fe844b9fc16ffe9ed02de1d7b
sassanghz <sassanghazi@gmail.com>
2024-11-06 20:09:39 -0500

    grid display functions

--- grid.py
+++ grid.py
@@ -29,6 +29,50 @@ class memory_grid:
         #list turns in a grid for display
         return [elements_grid[i * self.gridSize:(i + 1) * self.gridSize] for i in range(self.gridSize)]
 
+    def element_cell_reveal(self, row1, col1, row2, col2):
+        self.attemps += 1
+
+        if self.grid[row1][col1] == self.grid[row2][col2]:
+            # elements matched
+            self.hiddenGrid[row1][col1] = str(self.grid[row1][col1])
+            self.hiddenGrid[row2][col2] = str(self.grid[row2][col2])
+            self.foundPairs += 1
+            return True
+        else:
+            # elements != match
+            return False
     
+    def element_cell_hidden(self, row1, col1, row2, col2):
+        self.hiddenGrid[row1][col2] = 'X'
+        self.hiddenGrid[row2][col2] = 'X'
+    
+    def game_over(self):
+        # if all elements have been found
+        return self.foundPairs == self.pairsCount
+    
+    def display_grid(self):
+        header = "  "
+        
+        for i in range(self.gridSize):
+            #Appending each column letter with a space
+            header += chr(65 + i) + " "
+        print(header.strip())
+
+        #each row is printed with row indices
+        index_row = 0
+
+        for row in self.element_cell_hidden:
+            # Lines start with the row index in brackets
+            row_line = f"[{index_row}]"
+
+            for cell in row:
+                #Append each cell with a space
+                row_line += cell + " "
+            print(row_line.strip())
+            index_row += 1
+    
+    def final_score(self):
+        print(f"\nCongratulations! You've won! ")
+
 
 
